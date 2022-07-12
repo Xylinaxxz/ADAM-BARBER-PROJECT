d8a1b925d4eb0d53204da96a297905a1ee9e5e97
Ma-asuwan  cheheng <xylina.js@gmail.com>
2022-07-12 20:13:50 +0700

    TEST

---
+++ js/music-list.jsx
@@ -0,0 +1,57 @@
+// การเพิ่มเพลง ให้คัดลอกโค้ดต่อไปนี้แล้ววางลงในอาร์เรย์
+
+//   {
+//     name: "ชื้อเพลง",
+//     artist: "วง",
+//     img: "รูป",
+//     src: "เพลง"
+//   }
+
+//วางลงในอาร์เรย์ได้ไม่จำกัด
+
+let allMusic = [
+  {
+    name: "Live and Learn",
+    artist: "Non",
+    img: "Live and Learn",
+    src: "Live and Learn"
+  },
+  {
+    name: "ความรัก",
+    artist: "bodyslam",
+    img: "bodyslam",
+    src: "bodyslam"
+  },
+  {
+    name: "นาทีนี้",
+    artist: "Justin",
+    img: "natini",
+    src: "natini"
+  },
+  {
+    name: "นักเลงเก่า",
+    artist: "TaitosmitH",
+    img: "4king",
+    src: "4king"
+  },
+  {
+    name: "อย่างน้อย",
+    artist: "Big-ass",
+    img: "bis-ass-1",
+    src: "big-ass-1"
+  },
+  {
+    name: "คิดฮอด",
+    artist: "Bodyslam&1mill",
+    img: "1mill",
+    src: "1mill"
+  },
+  // การเพิ่มเพลง ให้คัดลอกโค้ดต่อไปนี้แล้ววางลงในอาร์เรย์
+
+//   {
+//     name: "ชื้อเพลง",
+//     artist: "วง",
+//     img: "รูป",
+//     src: "เพลง"
+//   }
+];
